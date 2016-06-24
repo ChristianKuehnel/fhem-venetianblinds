13701dc7071b6d91f6dea9fc380df05c948eefd5
Christian Kühnel <christian.kuehnel@gmail.com>
2016-06-24 20:50:02 +0200

    added oss licence

--- 99_Venetian2.pm
+++ 99_Venetian2.pm
@@ -1,3 +1,10 @@
+##############################################
+#
+# This is open source software licensed unter the Apache License 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+##############################################
+
 use v5.10.1;
 use strict;
 use warnings;
