838e1783306a152dc19a970a93d2b57457d642b7
Christian Kühnel <christian.kuehnel@gmail.com>
2016-07-02 18:56:07 +0200

    draft of motion model, untested

--- VenetianBlindController.pm
+++ VenetianBlindController.pm
@@ -12,11 +12,12 @@ use strict;
 use warnings;
 use experimental "smartmatch";
 
+# constants ########################
 
 my $scenes = {
 	"open" => {
         "blind" => 99,
-        "slat" => 99,   
+        "slat" => undef,   
     },
     "closed" => {
         "blind" => 0,
@@ -32,31 +33,52 @@ my $scenes = {
     },
 };
 
+my $blind_threshold =  5; #percentage points
+my $slat_threshold  =  5; #percentage points
+my $power_threshold = 10; #watts
+
+# FHEM commands ########################
+
 sub Define{
 	my ($hash,$a,$h) = @_;
 	$hash->{master_controller} = $h->{master};
 	$hash->{device} = $h->{device};
+	$hash->{could_index_threshold} = $h->{could_index_threshold};
+
 	my ($azstart,$azend) = split(/-/, $h->{azimuth});
 	$hash->{azimuth_start} = $azstart;
 	$hash->{azimuth_end} = $azend;
+
 	my ($evstart,$evend) = split(/-/, $h->{elevation});
 	$hash->{elevation_start} = $evstart;
 	$hash->{elevation_end} = $evend;
-	$hash->{current_scene} = undef;	
+
+	my ($monstart,$monend) = split(/-/, $h->{months});
+	$hash->{month_start} = $monstart;
+	$hash->{month_start} = $monend;
+
+
+	$hash->{automatic} = 0;	
+	$hash->{scene} = undef;	
 	return;
 }
 
 sub Set{
 	my ( $hash, $a,$h ) = @_;
 	my $cmd = $a->[1];
+	my @scene_list = keys %{$scenes};
 	if ( $cmd eq "?" ){
-		return "automatic:noArg wind_alarm:noArg"
+		my $result = "automatic:noArg wind_alarm:noArg";
+		foreach my $scene (@scene_list){
+			$result .= " $scene:noArg";
+		}
+		return $result;
 	} elsif ($cmd eq "automatic") {
 		$hash->{automatic} = 1;
 		update_automatic($hash);
-	} elsif ($cmd ~~ keys %{$hash->{scenes}}) {
+	} elsif ($cmd ~~ @scene_list) {
 		$hash->{automatic} = 0;
-		set_scene($hash,$cmd);
+		set_scene($hash, $cmd, 0);
 	} elsif ($cmd eq "scenes") {
 		delete $hash->{scences};
 	} elsif ($cmd eq "wind_alarm") {
@@ -76,23 +98,103 @@ sub Notify{
 	return;
 }
 
+
+# logic for blind control #####################
 sub update_automatic{
-	#TODO: get sun and wind params from Master Controller
-	#TODO: decide what to do
-	# if ($hash->{automatic})
-	#   set_secene (...)
+	my ($hash) = @_;
+	my $master = $hash->{master_controller};
+	my $sun_elevation = main::ReadingsVal($master, "sun_elevation", undef);
+	my $sun_azimuth = main::ReadingsVal($master, "sun_azimuth", undef);
+	my $wind_speed = main::ReadingsVal($master, "wind_speed", undef);
+	my $wind_alarm = main::ReadingsVal($master, "wind_alarm", undef);
+	my $cloud_index = main::ReadingsVal($master, "cloud_index", undef);
+	my $month = main::ReadingsVal($master, "month", undef);
+	
+	# reasons to not work in automatic mode
+	if ($wind_alarm  
+		or !$hash->{automatic} 
+		or $month < $hash->{month_start} 
+		or $month > $hash->{month_end} 	){ 
+			return; 
+	}
+	
+	my $new_scene = undef;
+	if ($hash->{elevation_start} <= $sun_elevation and 
+		$sun_elevation <= $hash->{end_elevation} and
+		$hash->{azimuth_start} <= $sun_azimuth and
+		$sun_azimuth <= $hash->{azimuth_end} and
+		$cloud_index <= $hash->{could_index_threshold}) {
+		$new_scene ="shaded";
+	} else {
+		$new_scene = "open";
+	}
+	
+	if (!($new_scene eq $hash->{scene})) {
+		set_scene($hash,$new_scene,0);
+	}
 }
 
+# move the blinds ##########################
 sub set_scene{
 	my ($hash,$scene,$force) = @_;
-	# if !wind_alarm
-	#TODO:get secene config
-	#TODO:move blinds
+	move_blinds($hash, $scenes->{scene}{blind}, $scenes->{scene}{slat});
+}
+
+sub move_blinds{
+	my ($hash, $blind, $slat)= @_;
+	my ($current_blind, $current_slat) = get_position($hash);
+	if ( abs($blind-$current_blind) > $blind_threshold ){
+		main::fhem("set $hash->{device} positionBlinds $blind");
+	}
+	if ( defined $slat and 
+		abs($slat-$current_slat) > $slat_threshold and
+		$blind < 95 ){
+		enqueue_command($hash,"set $hash->{device} positionSlat $slat");
+	}
 }
 
 sub wind_alarm{
 	my ($hash) = @_;
-	#if not open -> set position to open
+	move_blinds($hash,99,undef);
 }
 
+
+# queing of commans ######################
+sub enqueue_command {
+	my ($hash,$cmd) = @_;
+	$hash->{queue} = $cmd;
+	process_queue($hash);
+}
+
+sub process_queue {
+	my ($hash) = @_;
+	if (!defined $hash->{queue}) { return; };
+		
+	if (get_power($hash) > $power_threshold) {
+		main::InternalTimer(main::gettimeofday()+1, "VenetianBlindController::process_queue", $hash, 1);        
+	} else {
+		main::fhem($hash->{queue});
+		delete $hash->{queue}
+	}
+}
+
+
+# wrappers around readings #############################
+sub get_power{
+	my ($hash) = @_;
+    my $power_reading = main::ReadingsVal($hash->{device}, "power", undef);
+    $power_reading =~ /([\d\.]+)\WW/;
+    return $1;
+}
+
+sub get_position{
+	my ($hash) = @_;
+    main::fhem("get $hash->{device} position");
+    my $position = main::ReadingsVal($hash->{device}, "position", undef);
+    $position =~ /Blind (\d+) Slat (\d+)/;
+    if (!defined $1 or !defined $2){
+        main::Log( 1, "Error: could not get position of device $hash->{device}: $position");
+    }
+	return ($1,$2);
+}
 1;
